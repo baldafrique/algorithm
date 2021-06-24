b3aee428c5c1b22ece7dfbbc98f39f731cf56e89
Jason <jasonecy@gmail.com>
2021-06-24 22:47:33 +0900

    CodeUp 1090

--- src/algorithm/CodeUp1084.java
+++ src/algorithm/CodeUp1084.java
@@ -6,11 +6,20 @@ public class CodeUp1084 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int input = scanner.nextInt();
-		for(int i=1; i<=input; i++) {
-			if(i%3 != 0) System.out.print(i +" ");
-			else System.out.print("X ");
+		int red = scanner.nextInt();
+		int green = scanner.nextInt();
+		int blue = scanner.nextInt();
+		int count = 0;
+		
+		for(int i=0; i<red; i++) {
+			for(int j=0; j<green; j++) {
+				for(int k=0; k<blue; k++) {
+					System.out.println(i + " " + j + " " + k);
+					count++;
+				}
+			}
 		}
+		System.out.println(count);
 	}
 
 }
