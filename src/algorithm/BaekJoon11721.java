9fb1876f6df37f5583357618861a327a85266c0c
Jason <jasonecy@gmail.com>
2022-02-13 07:48:45 +0900

    Feb 13, 2022

--- src/algorithm/BaekJoon11721.java
+++ src/algorithm/BaekJoon11721.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon11721 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
