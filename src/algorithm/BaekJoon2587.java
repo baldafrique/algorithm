514796c8d303974dae101c3c6b0ac76aa5b64180
Jason <jasonecy@gmail.com>
2022-02-18 10:52:08 +0900

    Feb 18, 2022

--- src/algorithm/BaekJoon2587.java
+++ src/algorithm/BaekJoon2587.java
@@ -5,14 +5,22 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
-public class Main {
-	
+public class BaekJoon2587 {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		
+
+		int[] num = new int[5];
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			num[i] = Integer.parseInt(reader.readLine());
+			sum += num[i];
+		}
+		Arrays.sort(num);
+		writer.write(sum / 5 + "\n" + num[2]);
 		
 		writer.flush();
 		reader.close();
--- src/algorithm/BaekJoon5522.java
+++ src/algorithm/BaekJoon5522.java
@@ -6,13 +6,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon5522 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		
+
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			sum += Integer.parseInt(reader.readLine());
+		}
+		writer.write(Integer.toString(sum));
 		
 		writer.flush();
 		reader.close();
