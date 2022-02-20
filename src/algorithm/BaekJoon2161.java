0731e1febda301f679817a4af575d1a5c7aaf326
Jason <jasonecy@gmail.com>
2022-02-21 07:59:26 +0900

    Feb 21, 2022

--- src/algorithm/BaekJoon2161.java
+++ src/algorithm/BaekJoon2161.java
@@ -5,21 +5,32 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Main {
+public class BaekJoon2161 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int a = Integer.parseInt(reader.readLine());
-		int b = Integer.parseInt(reader.readLine());
-		writer.write(Integer.toString(b - a));
+		Queue<Integer> queue = new LinkedList<>();
+		
+		int num = Integer.parseInt(reader.readLine());
+		
+		for (int i = 0; i < num; i++) {
+			queue.add(i + 1);
+		}
+		
+		while (queue.size() > 1) {
+			writer.write(queue.poll() + " ");
+			queue.add(queue.poll());
+		}
+		writer.write(Integer.toString(queue.poll()));
 		
 		writer.flush();
 		reader.close();
 		writer.close();
-		
 	}
 
 }
--- src/algorithm/BaekJoon24078.java
+++ src/algorithm/BaekJoon24078.java
@@ -6,15 +6,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon24078 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int a = Integer.parseInt(reader.readLine());
-		int b = Integer.parseInt(reader.readLine());
-		writer.write(Integer.toString(b - a));
+		writer.write(Integer.toString(Integer.parseInt(reader.readLine()) % 21));
 		
 		writer.flush();
 		reader.close();
