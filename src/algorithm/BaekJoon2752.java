55d3d86adc971e0ed50febaaf986171c405f757c
Jason <jasonecy@gmail.com>
2022-03-02 08:11:31 +0900

    Mar 2, 2022

--- src/algorithm/BaekJoon2752.java
+++ src/algorithm/BaekJoon2752.java
@@ -5,14 +5,24 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
-public class Main {
+public class BaekJoon2752 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		String[] str = reader.readLine().split(" ");
+		int[] num = new int[3];
+		for (int i = 0; i < 3; i++) {
+			num[i] = Integer.parseInt(str[i]);
+		}
+		Arrays.sort(num);
 		
+		for (int i : num) {
+			writer.write(i + " ");
+		}
 		
 		writer.flush();
 		reader.close();
