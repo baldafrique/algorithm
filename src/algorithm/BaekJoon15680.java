092240ccbf553f66fcd6821e014c5b2b015dc88a
Jason <jasonecy@gmail.com>
2022-03-05 08:28:09 +0900

    Mar 5, 2022

--- src/algorithm/BaekJoon15680.java
+++ src/algorithm/BaekJoon15680.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon15680 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		
+		writer.write(reader.readLine().equals("0") ? "YONSEI" : "Leading the Way to the Future");
 		
 		writer.flush();
 		reader.close();
