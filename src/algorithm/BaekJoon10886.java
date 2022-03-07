1f0e4110c7be74834b0e58893bff54b6900a9b46
Jason <jasonecy@gmail.com>
2022-03-07 20:33:23 +0900

    Mar 7, 2022

--- src/algorithm/BaekJoon10886.java
+++ src/algorithm/BaekJoon10886.java
@@ -6,16 +6,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon10886 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-<<<<<<< HEAD
-
-=======
 		
->>>>>>> 7776261b8299162432b00f0a8b932c6f1c2acd6c
 		
 		
 		writer.flush();
