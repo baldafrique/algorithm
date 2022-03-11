9f62216da461f21f2b5239b0bed0ff6747994d74
Jason <jasonecy@gmail.com>
2022-03-11 22:07:11 +0900

    Mar 11, 2022

--- src/baekJoon/BaekJoon5988.java
+++ src/baekJoon/BaekJoon5988.java
@@ -1,18 +1,26 @@
-package main;
+package baekJoon;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 
-public class Main {
+public class BaekJoon5988 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
  		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
  		
+ 		int num = Integer.parseInt(reader.readLine());
+ 		BigInteger value;
+ 		BigInteger two = new BigInteger("2");
  		
+ 		for (int i = 0; i < num; i++) {
+ 			value = new BigInteger(reader.readLine());
+ 			writer.write((value.remainder(two).toString().equals("0") ? "even" : "odd") + "\n");
+ 		}
  		
  		writer.flush();
 		reader.close();
