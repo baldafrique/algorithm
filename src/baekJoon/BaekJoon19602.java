7cddbe974078476edcd7ab14ab6076d8c2e154c7
Jason <jasonecy@gmail.com>
2022-03-16 10:01:13 +0900

    Mar 16, 2022

--- src/baekJoon/BaekJoon19602.java
+++ src/baekJoon/BaekJoon19602.java
@@ -1,4 +1,4 @@
-package main;
+package baekJoon;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,14 +6,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon19602 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
  		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
  		
+ 		int s = Integer.parseInt(reader.readLine());
+ 		int m = Integer.parseInt(reader.readLine());
+ 		int l = Integer.parseInt(reader.readLine());
  		
+ 		writer.write(s + 2 * m + 3 * l >= 10 ? "happy" : "sad");
  		writer.flush();
 		reader.close();
 		writer.close();
