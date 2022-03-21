46190d06a5506c298f3fd0290f436cf951772a13
Jason <jasonecy@gmail.com>
2022-03-21 22:55:11 +0900

    Mar 21, 2022

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,9 +11,9 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
  		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
- 		
- 		
 
+
+ 		
  		writer.flush();
 		reader.close();
 		writer.close();
