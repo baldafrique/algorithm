c727a899291e56a7cf7cce08de62b526da943e2b
Jason <jasonecy@gmail.com>
2022-03-27 09:46:59 +0900

    Mar 27, 2022

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,12 +7,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
+	static int white = 0;
+	static int blue = 0;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
  		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
  		
+ 		// 배열로 입력받기
  		
+ 		// 0과 1이 섞여있는지 확인
+ 		
+ 		// 섞여있는 경우 4등분
  		
  		writer.flush();
 		reader.close();
