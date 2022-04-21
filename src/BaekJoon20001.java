75bed21a19cfd5bae3e71e0376875a49d60a6f0c
Jason <jasonecy@gmail.com>
2022-04-21 13:26:55 +0900

    Apr 21, 2022

--- src/BaekJoon20001.java
+++ src/BaekJoon20001.java
@@ -4,12 +4,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class BaekJoon10821 {
+public class BaekJoon20001 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		writer.write(Integer.toString(reader.readLine().split(",").length));
+		String input;
+		reader.readLine();
+		input = reader.readLine();
+		int cnt = 0;
+		
+		while (!input.equals("고무오리 디버깅 끝")) {
+			if (input.equals("문제")) {
+				cnt++;
+			}
+			else {
+				if (cnt == 0) {
+					cnt += 2;
+				}
+				else {
+					cnt--;
+				}
+			}
+			input = reader.readLine();
+		}
+		
+		writer.write(cnt == 0 ? "고무오리야 사랑해" : "힝구");
+		
 		
 		writer.flush();
 		reader.close();
