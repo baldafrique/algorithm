0a9f156dea9a7b66f14b0c196e9eab052b7e77c8
Jason <jasonecy@gmail.com>
2022-04-23 22:38:48 +0900

    Apr 23, 2022

---
+++ src/BaekJoon10814.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BaekJoon10814 {
+	private static class Person {
+		int age;
+		String name;
+		int order;
+		
+		private Person(int age, String name, int order) {
+			this.age = age;
+			this.name = name;
+			this.order = order;
+		}
+	}
+	
+	private static void swap(Person[] people, int i, int j) {
+		Person temp = people[i];
+		people[i] = people[j];
+		people[j] = temp;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(reader.readLine());
+		Person[] people = new Person[N];
+		StringTokenizer stringTokenizer;
+		
+		for (int i = 0; i < N; i++) {
+			stringTokenizer = new StringTokenizer(reader.readLine());
+			int age = Integer.parseInt(stringTokenizer.nextToken());
+			String name = stringTokenizer.nextToken();
+			people[i] = new Person(age, name, i + 1);
+		}
+		
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (people[i].age > people[j].age) {
+					swap(people, i, j);
+				}
+				else if (people[i].age == people[j].age && people[i].order > people[j].order) {
+					swap(people, i, j);
+				}
+			}
+		}
+		
+		for (Person person : people) {
+			writer.write(person.age + " " + person.name + "\n");
+		}
+		
+		writer.flush();
+		reader.close();
+		writer.close();
+	}
+}
--- src/BaekJoon2108.java
+++ src/BaekJoon2108.java
@@ -3,62 +3,73 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
-public class BaekJoon2108 { // unsolved
-
+public class BaekJoon2108 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(reader.readLine());
+		int[] numbers = new int[N];
+		int average = 0;
 		
-		@SuppressWarnings("unused")
-		int median, mode, range; // 중앙값, 최빈값, 범위
-		float mean = 0; // 평균
-		int num = Integer.parseInt(reader.readLine());
-		int[] log = new int[500000];
-		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			numbers[i] = Integer.parseInt(reader.readLine());
+			average += numbers[i];
+		}
+		
+		average = (int) Math.round((float) average / N);
+		Arrays.sort(numbers);
+		int median = numbers[N / 2];
+		int range = numbers[N - 1] - numbers[0];
+		
+		int[] count = new int[N];
+		int tempIndex = 0;
+		count[tempIndex] = 1;
+		int num = 1; // number of elements in count
+		
+		for (int i = 0; i < N - 1; i++) {
+			if (numbers[i] == numbers[i + 1]) {
+				count[tempIndex]++;
+			}
+			else {
+				count[++tempIndex] = 1;
+				num++;
+			}
+		}
+		
+		int maxCountIndex = 0;
+		for (int i = 0; i < N; i++) {
+			if (count[maxCountIndex] < count[i]) {
+				maxCountIndex = i;
+			}
+		}
+		
+		int tempMode = count[maxCountIndex];
+		boolean flag = false; 
+	
+		int maxModeIndex = 0;
 		for (int i = 0; i < num; i++) {
-			list.add(Integer.parseInt(reader.readLine()));
+			if (flag == true && count[i] == tempMode) {
+				maxModeIndex = i;
+				break;
+			}
+			else if (count[i] == tempMode) {
+				maxModeIndex = i;
+				flag = true;
+			}
 		}
-		Collections.sort(list);
 		
-		for (int i = 0; i < list.size(); i++) {
-			mean += list.get(i);
-			log[list.get(i) - 1]++;
-		}
-		int count = 0;
 		int maxIndex = 0;
-		int counter = 0;
-		for (int i : log) {
-			if (log[i] > count) {
-				maxIndex = i;
-			}
+		for (int i = 0; i < maxModeIndex; i++) {
+			maxIndex += count[i];
 		}
+
+		int mode = numbers[maxIndex];
 		
-		for (int i = 500000; i >= 0; i--) {
-			if (log[i] == log[maxIndex]) {
-				counter++;
-			}
-			if (counter > 1) {
-				// log[i]
-				writer.write(log[i] + "\n");
-			}
-		}
-		if (counter == 1) {
-			// log[maxIndex]
-			writer.write(log[maxIndex] + "\n");
-		}
-		
-		mean /= num;
-		median = list.get(num / 2);
-		range = list.get(num - 1) - list.get(0);
-		writer.write(Math.round(mean) + "\n");
-		writer.write(median + "\n");
-		writer.write(range + "\n");
+		writer.write(average + "\n" + median + "\n" + mode + "\n" + range);
 		writer.flush();
 		reader.close();
 		writer.close();
 	}
-
 }
