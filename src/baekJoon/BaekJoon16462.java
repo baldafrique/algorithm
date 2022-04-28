bf1f4e1d2ff80314ab8081b02fe0ae5d43a84dde
Jason <jasonecy@gmail.com>
2022-04-28 10:00:59 +0900

    Apr 28, 2022

--- src/baekJoon/BaekJoon16462.java
+++ src/baekJoon/BaekJoon16462.java
@@ -1,16 +1,27 @@
+package baekJoon;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon16462 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		int n = Integer.parseInt(reader.readLine());
+		double sum = 0;
 		
-		
+		for (int i = 0; i < n; i++) {
+			String input = reader.readLine();
+			if (input.length() != 3) {
+				input = input.replace("0", "9");
+				input = input.replace("6", "9");
+			}
+			int score = Integer.parseInt(input);
+			sum += score;
+		}
+		writer.write(Integer.toString((int) Math.round(sum / n)));
 		writer.flush();
 		reader.close();
 		writer.close();
--- src/baekJoon/BaekJoon18245.java
+++ src/baekJoon/BaekJoon18245.java
@@ -1,15 +1,26 @@
+package baekJoon;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon18245 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		String code = reader.readLine();
+		int i = 1;
 		
+		while (!code.equals("Was it a cat I saw?")) {
+			for (int j = 0; j < code.length(); j++) {
+				writer.write(code.charAt(j));
+				j += i;
+			}
+			writer.write("\n");
+			i++;
+			code = reader.readLine();
+		}
 		
 		writer.flush();
 		reader.close();
