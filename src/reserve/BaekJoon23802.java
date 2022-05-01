02ff1ee48619fffdd23edbf4924c6553cf066f37
Jason <jasonecy@gmail.com>
2022-05-01 18:36:57 +0900

    May 1, 2022

--- src/reserve/BaekJoon23802.java
+++ src/reserve/BaekJoon23802.java
@@ -1,15 +1,29 @@
+package reserve;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon23802 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(reader.readLine());
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 5 * N; j++) {
+				writer.write("@");
+			}
+			writer.write("\n");
+		}
 		
-		
+		for (int i = 0; i < 4 * N; i++) {
+			for (int j = 0; j < N; j++) {
+				writer.write("@");
+			}
+			writer.write("\n");
+		}
 		
 		reader.close();
 		writer.close();
--- src/reserve/BaekJoon23803.java
+++ src/reserve/BaekJoon23803.java
@@ -1,15 +1,28 @@
+package reserve;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon23803 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(reader.readLine());
 		
-		
+		for (int i = 0; i < N * 4; i++) {
+			for (int j = 0; j < N; j++) {
+				writer.write("@");
+			}
+			writer.write("\n");
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N * 5; j++) {
+				writer.write("@");	
+			}
+			writer.write("\n");
+		}
 		
 		reader.close();
 		writer.close();
