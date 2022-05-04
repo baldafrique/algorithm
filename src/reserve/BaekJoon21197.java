37ef0b60fc2c5ca02a0bf73f63a2e38b3af4d379
Jason <jasonecy@gmail.com>
2022-05-04 12:22:01 +0900

    May 4, 2022

--- src/reserve/BaekJoon21197.java
+++ src/reserve/BaekJoon21197.java
@@ -1,17 +1,29 @@
+package reserve;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon21197 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		int N = Integer.parseInt(reader.readLine());
+		boolean isRunning = false;
+		int time = 0;
+		int[] records = new int[N];
 		
+		for (int i = 0; i < N; i++) {
+			isRunning = !isRunning;
+			records[i] = Integer.parseInt(reader.readLine());
+			if (!isRunning) {
+				time += records[i] - records[i - 1];
+			}
+		}
 		
+		writer.write(isRunning ? "still running" : Integer.toString(time));
 		reader.close();
 		writer.close();
 	}
---
+++ src/reserve/BaekJoon7947.java
@@ -0,0 +1,36 @@
+package reserve;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BaekJoon7947 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		int z = Integer.parseInt(reader.readLine());
+		
+		for (int i = 0; i < z; i++) {
+			double red = 0;
+			double green = 0;
+			double blue = 0;
+			
+			for (int j = 0; j < 10; j++) {
+				StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
+				red += Integer.parseInt(stringTokenizer.nextToken());
+				green += Integer.parseInt(stringTokenizer.nextToken());
+				blue += Integer.parseInt(stringTokenizer.nextToken());
+			}
+			
+			writer.write(Math.round(red / 10) + " ");
+			writer.write(Math.round(green / 10) + " ");
+			writer.write(Math.round(blue / 10) + "\n");
+		}
+		
+		reader.close();
+		writer.close();
+	}
+}
