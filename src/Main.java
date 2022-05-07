633386d58d9f14576c6a09bfd1a2b9d809288297
Jason <jasonecy@gmail.com>
2022-05-07 20:51:05 +0900

    May 7, 2022

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		System.out.println("b".compareTo("b"));
+		
 		
 		reader.close();
 		writer.close();
