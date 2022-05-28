4f8b3db803f03a4a5727270c6c38f5aaf4ffb9e9
Jason <jasonecy@gmail.com>
2022-05-28 19:52:56 +0900

    May 28, 2022

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,13 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T = Integer.parseInt(reader.readLine());
-		for (int i = 0; i < T; i++) {
-			int N = Integer.parseInt(reader.readLine());
-			int[] numbers = new int[N];
-			StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
-			for (int j = 0; j < N; j++) {
-				numbers[j] = Integer.parseInt(stringTokenizer.nextToken());
-			}
-			Arrays.sort(numbers);
-			writer.write(numbers[0] + " " + numbers[N - 1] + "\n");
-		}
+
 		
 		
 		reader.close();
