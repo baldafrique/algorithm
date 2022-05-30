23d4db08860c191e18b2080b65ac58c45ee54cc1
Jason <jasonecy@gmail.com>
2022-05-30 17:15:34 +0900

    May 30, 2022

--- src/reserve/BaekJoon20833.java
+++ src/reserve/BaekJoon20833.java
@@ -1,17 +1,23 @@
+package reserve;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon20833 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(reader.readLine());
+		long sum = 0;
 		
+		for (int i = 1; i <= N; i++) {
+			sum += Math.pow(i, 3);
+		}
 		
-		
+		writer.write(Long.toString(sum));
 		reader.close();
 		writer.close();
 	}
--- src/reserve/BaekJoon8658.java
+++ src/reserve/BaekJoon8658.java
@@ -1,17 +1,30 @@
+package reserve;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon8658 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(reader.readLine());
 		
+		for (int i = 2; i < n; i++) {
+			if (n % i != 0) {
+				writer.write(i + " ");
+				break;
+			}
+		}
 		
-		
+		for (int i = n - 1; i > 1; i--) {
+			if (n % i != 0) {
+				writer.write(i + " ");
+				break;
+			}
+		}
 		reader.close();
 		writer.close();
 	}
