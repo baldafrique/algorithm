a4e262b27ae1c5dab75bd63f00d60f135746a8ba
Jason <jasonecy@gmail.com>
2022-06-05 22:00:22 +0900

    June 5, 2022

--- src/reserve/BaekJoon23037.java
+++ src/reserve/BaekJoon23037.java
@@ -1,17 +1,23 @@
+package reserve;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class BaekJoon23037 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		String number = reader.readLine();
+		int sum = 0;
 		
+		for (int i = 0; i < 5; i++) {
+			sum += Math.pow(number.charAt(i) - '0', 5);
+		}
 		
+		writer.write(Integer.toString(sum));
 		reader.close();
 		writer.close();
 	}
