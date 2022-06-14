0adcd0857785e22713cb2237a36e569941beb265
Jason <jasonecy@gmail.com>
2022-06-14 12:21:50 +0900

    Jun 14, 2022

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,13 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
-		
+
 		
 		
 		writer.flush();
