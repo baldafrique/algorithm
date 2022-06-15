608617906a02d0276ca8f62b536010014393efdc
Jason <jasonecy@gmail.com>
2022-06-15 23:22:31 +0900

    Jun 15, 2022

--- src/baekJoon/BaekJoon1264.java
+++ src/baekJoon/BaekJoon1264.java
@@ -1,16 +1,28 @@
-package reserve;
+package baekJoon;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class BaekJoon11282 {
-
+public class BaekJoon1264 {
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		writer.write((char) (Integer.parseInt(reader.readLine()) + 44031));
+		String vowels = "AaEeIiOoUu";
+		String sentence = reader.readLine();
+		while (!sentence.equals("#")) {
+			int number = 0;
+			for (int i = 0; i < sentence.length(); i++) {
+				if (vowels.contains(sentence.substring(i, (i + 1)))) {
+					number++;
+				}
+			}
+			writer.write(number + "\n");
+			sentence = reader.readLine();
+		}
+		
 		reader.close();
 		writer.close();
 	}
