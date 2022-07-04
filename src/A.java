d111a11b07079cfc6136ee24e58bad6c5e987274
Jason <jasonecy@gmail.com>
2022-07-04 09:10:21 +0900

    Jul 4, 2022

--- src/A.java
+++ src/A.java
@@ -4,15 +4,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class A {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		int N = (int) Math.ceil(Double.parseDouble(reader.readLine()) / 4);
 		
+		for (int i = 0; i < N; i++) {
+			writer.write("long ");
+		}
 		
-		
+		writer.write("int");
 		writer.flush();
 		reader.close();
 		writer.close();
--- src/baekJoon/BaekJoon17903.java
+++ src/baekJoon/BaekJoon17903.java
@@ -1,4 +1,5 @@
-package reserve;
+package baekJoon;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
