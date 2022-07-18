c7fea68b6e7cd963ebb3615a1240570f69198a11
Jason <jasonecy@gmail.com>
2022-07-18 09:41:45 +0900

    Jul 18, 2022

--- src/outstanding/BaekJoon21631.java
+++ src/outstanding/BaekJoon21631.java
@@ -1,19 +1,24 @@
-package reserve;
+package outstanding;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-public class BaekJoon21633 {
+public class BaekJoon21631 {
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		double commission = 25 + Integer.parseInt(reader.readLine()) * 0.01;
-		System.out.printf("%.2f", commission < 100 ? 100 : commission > 2000 ? 2000 : commission);
+		StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
+		int a = Integer.parseInt(stringTokenizer.nextToken());
+		int b = Integer.parseInt(stringTokenizer.nextToken());
 		
+		writer.write(Integer.toString(a < b ? a + 1 : a));
 		writer.flush();
 		reader.close();
 		writer.close();
 	}
+
 }
