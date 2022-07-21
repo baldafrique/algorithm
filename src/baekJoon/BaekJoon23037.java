629ca9e04823e0df332afdd06c21af280d6fa4da
Jason <jasonecy@gmail.com>
2022-07-21 13:56:34 +0900

    Jul 21, 2022

--- src/baekJoon/BaekJoon23037.java
+++ src/baekJoon/BaekJoon23037.java
@@ -6,23 +6,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class BaekJoon23037 {
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		long n = Long.parseLong(reader.readLine());
-		long sum = 0;
-		long quotient = 10;
+		String number = reader.readLine();
+		int sum = 0;
 		
 		for (int i = 0; i < 5; i++) {
-			long remainder = n % quotient; 
-			sum += Math.pow(remainder, 5);
-			n /= 10;
+			sum += Math.pow(number.charAt(i) - '0', 5);
 		}
 		
-		writer.write(String.valueOf(sum));
-		writer.flush();
+		writer.write(Integer.toString(sum));
 		reader.close();
 		writer.close();
 	}
