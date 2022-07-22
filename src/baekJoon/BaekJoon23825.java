5a8740a2eaf27ae133e97165cdd61c2f49d6c5ef
Jason <jasonecy@gmail.com>
2022-07-22 10:56:36 +0900

    Jul 22, 2022

--- src/baekJoon/BaekJoon23825.java
+++ src/baekJoon/BaekJoon23825.java
@@ -7,15 +7,24 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class BaekJoon23825 {
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer stringTokenizer = new StringTokenizer(reader.readLine());
 		int N = Integer.parseInt(stringTokenizer.nextToken());
 		int M = Integer.parseInt(stringTokenizer.nextToken());
-		writer.write(Integer.toString(Math.min(N, M) / 2));
+		int num = 0;
+		
+		while (N >= 2 && M >= 2) {
+			N -= 2;
+			M -= 2;
+			num++;
+		}
+		
+		writer.write(Integer.toString(num));
 		reader.close();
 		writer.close();
 	}
+
 }
